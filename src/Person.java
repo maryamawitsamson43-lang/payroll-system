b54532b1ab7847ceda0c5502503d6ed18806fa88
milkaaraya <arayamilka127@gmail.com>
2026-01-28 18:29:15 +0300

    Wrote methods on the Person class.

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,29 @@
 public abstract class Person {
     private String name;
+    private String personID;
     private String password;
 
+    public Person(String name, String personID, String password) {
+        this.name = name;
+        this.personID = personID;
+        this.password = password;
+    }
+
+    public boolean login(String inputPassword) {
+        return password.equals(inputPassword);
+    }
+
+    public void changePassword(String oldPass, String newPass) {
+        if (password.equals(oldPass)) {
+            password = newPass;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPersonID() {
+        return personID;
+    }
 }
